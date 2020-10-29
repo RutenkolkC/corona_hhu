dab06d79a720315e53a5ea6b9512924359d92e5b
Chris Rutenkolk <chris.rutenkolk@hhu.de>
2020-10-29 18:15:48 +0100

    lululu

---
+++ pull_website.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+wget --reject pdf,svg,eml,png,json,xlsx,pptx,eot,eot2,ttf,woff,woff2 --recursive --level=1 --span-hosts --domains=corona.hhu.de --random-wait  --mirror --convert-links --adjust-extension --page-requisites --no-parent https://corona.hhu.de
+git add --all
+git commit -m $(date)
