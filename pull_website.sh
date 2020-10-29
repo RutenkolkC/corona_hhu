b8013f139d834fcb305cb73a6457216b21f48d89
Chris Rutenkolk <chris.rutenkolk@hhu.de>
2020-10-29 18:23:20 +0100

    Do 29. Okt 18:23:20 CET 2020

--- pull_website.sh
+++ pull_website.sh
@@ -2,4 +2,4 @@
 wget --reject pdf,svg,eml,png,json,xlsx,pptx,eot,eot2,ttf,woff,woff2 --recursive --level=1 --span-hosts --domains=corona.hhu.de --random-wait  --mirror --convert-links --adjust-extension --page-requisites --no-parent https://corona.hhu.de
 git add --all
 git commit -m "$(date)"
-git push origin master
+git push origin main 
