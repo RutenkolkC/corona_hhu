6fe83a7cbd7e041b6c8be7b1081c5c48d57e9c92
Chris Rutenkolk <chris.rutenkolk@hhu.de>
2020-10-30 11:28:02 +0100

    Fr 30. Okt 11:28:02 CET 2020

--- pull_website.sh
+++ pull_website.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 wget --reject pdf,svg,eml,png,json,xlsx,pptx,eot,eot2,ttf,woff,woff2 --recursive --level=1 --span-hosts --domains=corona.hhu.de --random-wait  --mirror --convert-links --adjust-extension --page-requisites --no-parent https://corona.hhu.de
 find . -type f -name "*.html" -exec tidy -m '{}' \;
 find . -type f -name "*.html" -exec ./to_plaintext.py '{}' \;
-find ./* -type f -not -name "README.adoc" -not -name "pull_website.sh" -not -name "to_plaintext.py" -not -name "*.md" -not -path "./.git/*" -exec echo '{}' \;
+find ./* -type f -not -name "README.adoc" -not -name "pull_website.sh" -not -name "to_plaintext.py" -not -name "*.md" -not -path "./.git/*" -exec rm '{}' \;
 git add --all
 git commit -m "$(date)"
 #git push origin main 
